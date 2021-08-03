73a8a4ab1319e68c36405b39514a1f82cc9146e6
Artemleonenkoav <leonenkoavfin@gmail.com>
2021-08-03 13:08:21 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte a = 1;
+        short b = 2;
+        long c = 2121;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
